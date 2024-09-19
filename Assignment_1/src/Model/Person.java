87d910a1e3bafe07cdb3f70b9ba1bb708af104ec
mansiikamble <kamble.ma@northeastern.edu>
2024-09-19 16:52:36 -0400

    final

---
+++ Assignment_1/src/Model/Person.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author mansi kamble
+ */
+public class Person {
+   
+    String Fname;
+    String Lname;
+    String Email;
+    String Phone;
+    String DLnumber;
+    String DLstate;
+    String SSN;
+    String Address1;
+    String Address2;
+    String City;
+    String State;
+    String Zip;
+    String Telephone;
+    String Fax;
+    String DOB;
+    String Age;
+    String School;
+    String Gradyear;
+    String Hobby1;
+    String Hobby2;
+    
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDLnumber() {
+        return DLnumber;
+    }
+
+    public void setDLnumber(String DLnumber) {
+        this.DLnumber = DLnumber;
+    }
+
+    public String getDLstate() {
+        return DLstate;
+    }
+
+    public void setDLstate(String DLstate) {
+        this.DLstate = DLstate;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String Address1) {
+        this.Address1 = Address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String Address2) {
+        this.Address2 = Address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public String getFax() {
+        return Fax;
+    }
+
+    public void setFax(String Fax) {
+        this.Fax = Fax;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getSchool() {
+        return School;
+    }
+
+    public void setSchool(String School) {
+        this.School = School;
+    }
+
+    public String getGradyear() {
+        return Gradyear;
+    }
+
+    public void setGradyear(String Gradyear) {
+        this.Gradyear = Gradyear;
+    }
+
+    public String getHobby1() {
+        return Hobby1;
+    }
+
+    public void setHobby1(String Hobby1) {
+        this.Hobby1 = Hobby1;
+    }
+
+    public String getHobby2() {
+        return Hobby2;
+    }
+
+    public void setHobby2(String Hobby2) {
+        this.Hobby2 = Hobby2;
+    }
+    
+}
